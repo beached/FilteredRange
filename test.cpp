7a4c8ba536d8fac1b1d318cd84e6d9a1f57660b0
Darrell Wright <dwright-git@dawdevel.ca>
2014-10-19 22:14:25 -0400

    Cleaned up test

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ int main( int argc, char **arv ) {
 	std::vector<int> test_values = { 100, 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10, 10, 11, 55, 3, 7, 22, 11 };
 	std::vector<int> test_values2 = { 200, 201, 201, 199, 220 };
 	
-	auto on_each = []( int x ) {
+	auto show_int = []( int x ) {
 		std::cout << x << "\n";
 	};
 
@@ -20,12 +20,13 @@ int main( int argc, char **arv ) {
 		return x < 10;
 	};
 
-	auto tmp = daw::range::make_filtered_range( test_values ).where( is_even ).where( under_ten ).for_each( on_each ).clear_where( );
-	std::cout << "---\n";
-	auto tmp2 = tmp.append( test_values2.begin( ), test_values2.end( ) );
-	std::cout << "---\n";
-	tmp2.stable_unique( ).for_each( on_each );
-	std::cout << "---\n";
+	auto tmp = daw::range::make_filtered_range( test_values )
+		.where( is_even )
+		.where( under_ten )
+		.for_each( show_int ).clear_where( )
+		.append( test_values2.begin( ), test_values2.end( ) )
+		.stable_unique( ).for_each( show_int );
+
 	system( "PAUSE" );
 	return EXIT_SUCCESS;
 }
